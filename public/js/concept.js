5a8cefd44a5c7b232c5923fd2c545fb13a043b95
Todd Nichols <nivram80@gmail.com>
2015-05-10 08:34:39 -0500

    create dropdown navigation

--- public/js/concept.js
+++ public/js/concept.js
@@ -1,1 +1,15 @@
-// var el = document.getElementById('')
+var menuToggle = document.getElementById('menu-toggle');
+var mobileNav = document.getElementById('mobile-nav');
+
+function toggle(displayStatus) {
+	if (displayStatus === 'none') {
+		mobileNav.style.display = 'block';
+	} else {
+		mobileNav.style.display = 'none';
+	}
+}
+
+menuToggle.addEventListener('click', function() {
+	var displayStatus = window.getComputedStyle(mobileNav)['display'];
+	toggle(displayStatus);
+});
