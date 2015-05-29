2a564e336815a6d0ae925b17fd91f2dedca1408d
Todd Nichols <nivram80@gmail.com>
2015-05-28 20:02:15 -0500

    home page adjustments

--- public/js/concept.js
+++ public/js/concept.js
@@ -1,15 +1,7 @@
-var menuToggle = document.getElementById('menu-toggle');
-var mobileNav = document.getElementById('mobile-nav');
+$(document).ready(function() {
 
-function toggle(displayStatus) {
-	if (displayStatus === 'none') {
-		mobileNav.style.display = 'block';
-	} else {
-		mobileNav.style.display = 'none';
-	}
-}
+$('#menu-toggle').click(function() {
+	$('#mobile-nav').slideToggle('slow');
+});
 
-menuToggle.addEventListener('click', function() {
-	var displayStatus = window.getComputedStyle(mobileNav)['display'];
-	toggle(displayStatus);
 });
