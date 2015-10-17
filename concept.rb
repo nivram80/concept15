d73edcd15ae0909718ab18a0136997157f5aa97e
Todd Nichols <nivram80@gmail.com>
2015-10-17 13:57:16 -0500

    add photos; contact us button; more styling

--- concept.rb
+++ concept.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 
-set :protection, :except => :frame_options
+# set :protection, :except => :frame_options
 
 get '/' do
   erb :index
--- public/js/concept.js
+++ public/js/concept.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 
 $('#menu-toggle').click(function() {
 	$('#mobile-nav').slideToggle('slow');
+	console.log($('#mobile-nav'));
+	console.log($('#mobile-nav').css('display') == 'block');
 });
+
+// if ($('#mobile-nav').css('display') == 'block') {
+// 	console.log($('#mobile-nav').css('display'));
+// 	$('html').click(function() {
+// 		$('#mobile-nav').slideUp('slow');
+// 	});
+// }
+
 
 });
